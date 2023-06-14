de7122fde3e39b7606a330f31084d292a550d1be
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-06-14 10:19:54 +0200

    Add loadSong function & array with songs objects

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('pref');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+    {
+        name: 'Skillet',
+        displayName: 'Whispers In The Dark',
+        artist: 'Skillet',
+    },
+    {
+        name: '30sToMars',
+        displayName: 'The Kill',
+        artist: 'Thirty Seconds To Mars',
+    },
+    {
+        name: 'MGK',
+        displayName: 'Baddest',
+        artist: 'Machine Gun Kelly',
+    },
+    {
+        name: 'ThreeDaysGrace',
+        displayName: 'I Hate Everything About You',
+        artist: 'Three Days Grace',
+    },
+    {
+        name: 'FallingInReverse',
+        displayName: 'Watch The World Burn',
+        artist: 'Falling In Reverse',
+    },
+];
 
 let isPlaying = false;
 
@@ -22,3 +54,12 @@ function pauseSong() {
 }
 
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`
+    image.src = `img/${song.name}.jpg`
+}
+
+loadSong(songs[0]);
