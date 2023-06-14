4c203ad1aeb14024c69dda12527358bd5211f233
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-06-14 11:45:36 +0200

    Display correct duration of the song

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container');
 const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('pref');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -91,6 +93,16 @@ function updateProgressBar(event) {
         const { duration, currentTime } = event.srcElement;
         const progressPercent = (currentTime / duration) * 100;
         progress.style.width = `${progressPercent}%`;
+        // Calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        // Delay switching duration Element to avoid NaN
+        if (durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
     }
 }
 
